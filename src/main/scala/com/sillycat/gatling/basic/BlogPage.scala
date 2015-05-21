779bf6a1d0134846804aaa1d811baf68729b8a9b
luohuazju <luohuazju@gmail.com>
2015-05-21 15:28:58 -0500

    move them from test to main

--- src/main/scala/com/sillycat/gatling/basic/BlogPage.scala
+++ src/main/scala/com/sillycat/gatling/basic/BlogPage.scala
@@ -1,33 +1,27 @@
 package com.sillycat.gatling.basic
 
-import com.sillycat.gatling.GatlingBaseApp
-import com.typesafe.config.ConfigFactory
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 /**
  * Created by carl on 5/20/15.
  */
-
-
-class TaobaoHomePage extends Simulation{
-
+class BlogPage extends Simulation {
   // your code starts here
   val httpConf = http
-    .baseURL("http://www.taobao.com")
+    .baseURL("http://sillycat.iteye.com")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .doNotTrackHeader("1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("Taobao Scenario")
-    .exec(http("Home Page")
-    .get("/index_global.php"))
+  val scn = scenario("Blog Scenario")
+    .exec(http("Blog Page")
+    .get("/blog/2213025"))
 
   setUp(
     scn.inject(atOnceUsers(5))
   ).protocols(httpConf)
   // your code ends here
-
 }
